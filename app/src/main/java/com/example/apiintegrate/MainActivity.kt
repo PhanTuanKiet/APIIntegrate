12ecab8bf70a98114cd865763b228b972d2d6ccd
Tuan Kiet Phan <tuankiet1610@gmail.com>
2019-12-26 17:40:57 +0700

    add rxjava call

--- app/src/main/java/com/example/apiintegrate/MainActivity.kt
+++ app/src/main/java/com/example/apiintegrate/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.apiintegrate
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,12 +7,17 @@ import com.example.apiintegrate.model.BaseRespone
 import com.example.apiintegrate.model.UserInfo
 import com.example.apiintegrate.network.APIService
 import com.example.apiintegrate.network.APIUtils
+import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,21 +40,29 @@ class MainActivity : AppCompatActivity() {
 
 
     fun getUserInfo() {
-        mAPIService.getUserInfo(USERNAME).enqueue(
-            object : Callback<BaseRespone>{
-                override fun onFailure(call: Call<BaseRespone>, t: Throwable) {
-                    Log.i("get API failed",t.message.toString());
-                }
+//        mAPIService.getUserInfo(USERNAME).enqueue(
+//            object : Callback<BaseRespone>{
+//                override fun onFailure(call: Call<BaseRespone>, t: Throwable) {
+//                    Log.i("get API failed",t.message.toString());
+//                }
+//
+//                override fun onResponse(call: Call<BaseRespone>, response: Response<BaseRespone>) {
+//                    runOnUiThread {
+//                        tvName.text = response.body()!!.user[0].name
+//                        tvBio.text = response.body()!!.user[0].bio
+//                    }
+//
+//                }
+//            }
+//        )
 
-                override fun onResponse(call: Call<BaseRespone>, response: Response<BaseRespone>) {
-                    runOnUiThread {
-                        tvName.text = response.body()!!.user[0].name
-                        tvBio.text = response.body()!!.user[0].bio
-                    }
+        var userObservable : Observable<BaseRespone> = mAPIService.getUserInfo(USERNAME)
 
-                }
-            }
-        )
-
+        userObservable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                tvName.text = it.user[0].name
+                tvBio.text = it.user[0].bio
+            },Throwable::printStackTrace)
     }
 }
--- app/src/main/java/com/example/apiintegrate/network/APIService.kt
+++ app/src/main/java/com/example/apiintegrate/network/APIService.kt
@@ -1,11 +1,8 @@
 package com.example.apiintegrate.network
 
 import com.example.apiintegrate.model.BaseRespone
-import com.example.apiintegrate.model.UserInfo
 import io.reactivex.Observable
-import retrofit2.Call
 import retrofit2.http.*
-import java.util.*
 
 interface APIService {
 
@@ -14,6 +11,6 @@ interface APIService {
         "Client-ID:4mvsf8yizhms3aow1lnicx8fyrsbwt"
     )
     @GET("users")
-     fun getUserInfo (@Query("login") userName: String) : Call<BaseRespone>
+     fun getUserInfo (@Query("login") userName: String) : Observable<BaseRespone>
 
 }
